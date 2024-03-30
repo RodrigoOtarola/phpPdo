cdfcbce42318a3a3a6f46b8d89da4aca45c8aa72
RodrigoOtarola <rodrigo.otarola.gutierrez@ciisa.cl>
2024-03-29 22:43:42 -0300

    Add app.php and server.php

---
+++ config/app.php
@@ -0,0 +1,9 @@
+<?php
+
+const SERVERURL = "http://localhost/phpMVC/";
+const COMPANY = "SISTEMA PRESTAMOS";
+const MONEDA = "$";
+date_default_timezone_set("America/Santiago");
+
+
+
---
+++ config/server.php
@@ -0,0 +1,14 @@
+<?php
+
+const SERVER="localhost";
+const DB="prestamos";
+const USER="root";
+const PASS="";
+
+//Para conectarse a la base de datos
+const SGBD="mysql:host=".SERVER.";dbname=".DB;
+
+//Encriptación
+const METHOD="AES-256-CBC";
+const SECRET_KEY='$PRESTAMOS@2020';
+CONST secret_iv='037970';
