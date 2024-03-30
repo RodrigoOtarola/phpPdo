5d133dacd8dcf97d9352d637e09ed9ec8fb9374d
RodrigoOtarola <rodrigo.otarola.gutierrez@ciisa.cl>
2024-03-30 20:45:17 -0300

    refactor code /view/plantilla.php

---
+++ view/plantilla.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title><?php echo COMPANY?></title>
+
+    <?php include "./view/inc/Link.php"?>
+
+</head>
+<body>
+
+	<!-- Main container -->
+	<main class="full-box main-container">
+
+        <?php include "./view/inc/NavLateral.php"; ?>
+
+
+		<!-- Page content -->
+		<section class="full-box page-content">
+            <?php include "./view/inc/Navbar.php"?>
+
+		</section>
+	</main>
+
+    <?php include "./view/inc/script.php"?>
+
+</body>
+</html>
