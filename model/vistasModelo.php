aa805363a5b222017fed4b50862e1f738318118f
RodrigoOtarola <rodrigo.otarola.gutierrez@ciisa.cl>
2024-03-31 17:09:37 -0300

    update links view/contenidos/*

--- model/vistasModelo.php
+++ model/vistasModelo.php
@@ -1,23 +1,27 @@
 <?php
 
-class vistasModelo{
+class vistasModelo
+{
 
     /** Modelo para obtener vistas */
-    protected static function obtener_vistas_modelo($vistas){
-        $listaBlanca=["home","client-list"];
+    protected static function obtener_vistas_modelo($vistas)
+    {
+        $listaBlanca = ["client-list", "client-new", "client-search", "client-update", "company", "home", "item-list",
+            "item-new", "item-search", "item-update", "reservation-list", "reservation-new", "reservation-pending",
+            "reservation-search", "reservation-update", "user-list", "reservation-reservation", "user-new", "user-search", "user-update"];
         //Comprueba si valor esta en array de datos, 1er param es el valor y 2do param es el arreglo
-        if(in_array($vistas, $listaBlanca)){
+        if (in_array($vistas, $listaBlanca)) {
             //is_file= comprueba si exista un archivo, mediante url entregada
-            if (is_file("./view/contenidos/".$vistas."-view.php")){
-                $contenido="./view/contenidos/".$vistas."-view.php";
-            }else{
-                $contenido="404";
+            if (is_file("./view/contenidos/" . $vistas . "-view.php")) {
+                $contenido = "./view/contenidos/" . $vistas . "-view.php";
+            } else {
+                $contenido = "404";
             }
 
-        }elseif ($vistas =="login" || $vistas == "index"){
-            $contenido="login";
-        }else{
-            $contenido="404";
+        } elseif ($vistas == "login" || $vistas == "index") {
+            $contenido = "login";
+        } else {
+            $contenido = "404";
         }
 
         return $contenido;
--- view/inc/Navbar.php
+++ view/inc/Navbar.php
@@ -2,7 +2,7 @@
     <a href="#" class="float-left show-nav-lateral">
         <i class="fas fa-exchange-alt"></i>
     </a>
-    <a href="<?php echo SERVERURL;?>view/user-update.html">
+    <a href="<?php echo SERVERURL;?>view/user-update/">
         <i class="fas fa-user-cog"></i>
     </a>
     <a href="#" class="btn-exit-system">
