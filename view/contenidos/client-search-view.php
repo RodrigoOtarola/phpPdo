86825443fd0b59f23ba1a5441f6542fd1c23d0c5
RodrigoOtarola <rodrigo.otarola.gutierrez@ciisa.cl>
2024-03-31 16:33:06 -0300

    Add inc/contendos/*

--- view/contenidos/client-search-view.php
+++ view/contenidos/client-search-view.php
@@ -1,6 +1,6 @@
 <div class="full-box page-header">
 	<h3 class="text-left">
-		<i class="fas fa-clipboard-list fa-fw"></i> &nbsp; LISTA DE CLIENTES
+		<i class="fas fa-search fa-fw"></i> &nbsp; BUSCAR CLIENTE
 	</h3>
 	<p class="text-justify">
 		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit nostrum rerum animi natus beatae ex. Culpa blanditiis tempore amet alias placeat, obcaecati quaerat ullam, sunt est, odio aut veniam ratione.
@@ -13,13 +13,55 @@
 			<a href="client-new.html"><i class="fas fa-plus fa-fw"></i> &nbsp; AGREGAR CLIENTE</a>
 		</li>
 		<li>
-			<a class="active" href="client-list.html"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; LISTA DE CLIENTES</a>
+			<a href="client-list.html"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; LISTA DE CLIENTES</a>
 		</li>
 		<li>
-			<a href="client-search.html"><i class="fas fa-search fa-fw"></i> &nbsp; BUSCAR CLIENTE</a>
+			<a class="active" href="client-search.html"><i class="fas fa-search fa-fw"></i> &nbsp; BUSCAR CLIENTE</a>
 		</li>
 	</ul>	
 </div>
+
+<div class="container-fluid">
+	<form class="form-neon" action="">
+		<div class="container-fluid">
+			<div class="row justify-content-md-center">
+				<div class="col-12 col-md-6">
+					<div class="form-group">
+						<label for="inputSearch" class="bmd-label-floating">¿Qué cliente estas buscando?</label>
+						<input type="text" class="form-control" name="busqueda-" id="inputSearch" maxlength="30">
+					</div>
+				</div>
+				<div class="col-12">
+					<p class="text-center" style="margin-top: 40px;">
+						<button type="submit" class="btn btn-raised btn-info"><i class="fas fa-search"></i> &nbsp; BUSCAR</button>
+					</p>
+				</div>
+			</div>
+		</div>
+	</form>
+</div>
+
+
+<div class="container-fluid">
+	<form action="">
+		<input type="hidden" name="eliminar-busqueda" value="eliminar">
+		<div class="container-fluid">
+			<div class="row justify-content-md-center">
+				<div class="col-12 col-md-6">
+					<p class="text-center" style="font-size: 20px;">
+						Resultados de la busqueda <strong>“Buscar”</strong>
+					</p>
+				</div>
+				<div class="col-12">
+					<p class="text-center" style="margin-top: 20px;">
+						<button type="submit" class="btn btn-raised btn-danger"><i class="far fa-trash-alt"></i> &nbsp; ELIMINAR BÚSQUEDA</button>
+					</p>
+				</div>
+			</div>
+		</div>
+	</form>
+</div>
+
 
 <div class="container-fluid">
 	<div class="table-responsive">
