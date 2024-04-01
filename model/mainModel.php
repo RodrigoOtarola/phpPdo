f6c40228724656b63391e0fe2a9412e652b61041
RodrigoOtarola <rodrigo.otarola.gutierrez@ciisa.cl>
2024-03-31 22:06:54 -0300

    Add method encryption and limpiar_cadena

--- model/mainModel.php
+++ model/mainModel.php
@@ -60,4 +60,40 @@ class mainModel
         return $letra."-".$numero;
     }
 
+    /** Funcion para limpíar cadenas */
+    protected static function limpiar_cadena($cadena)
+    {
+        //str_ireplace: Reemplaza el 1er valor por el 2do valor, y 3er parametro es la cadena de texto
+        $cadena=str_ireplace("<script>","",$cadena);
+        $cadena=str_ireplace("</script>","",$cadena);
+        $cadena=str_ireplace("<script src>","",$cadena);
+        $cadena=str_ireplace("<script type=>","",$cadena);
+        $cadena=str_ireplace("SELECT * FROM","",$cadena);
+        $cadena=str_ireplace("DELETE FROM","",$cadena);
+        $cadena=str_ireplace("INSERT INTO","",$cadena);
+        $cadena=str_ireplace("DROP TABLE","",$cadena);
+        $cadena=str_ireplace("DROP DATABASE","",$cadena);
+        $cadena=str_ireplace("TRUNCATE TABLE","",$cadena);
+        $cadena=str_ireplace("SHOW TABLES","",$cadena);
+        $cadena=str_ireplace("SHOW DATABASES","",$cadena);
+        $cadena=str_ireplace("<?php","",$cadena);
+        $cadena=str_ireplace("?>","",$cadena);
+        $cadena=str_ireplace("--","",$cadena);
+        $cadena=str_ireplace(">","",$cadena);
+        $cadena=str_ireplace("<","",$cadena);
+        $cadena=str_ireplace("[","",$cadena);
+        $cadena=str_ireplace("]","",$cadena);
+        $cadena=str_ireplace("^","",$cadena);
+        $cadena=str_ireplace("==","",$cadena);
+        $cadena=str_ireplace(";","",$cadena);
+        $cadena=str_ireplace("::","",$cadena);
+
+        //trim: Elimina espacios dentro de la cadena
+        $cadena=trim($cadena);
+
+        //Elimina cadenas invertidas
+        $cadena=stripcslashes($cadena);
+
+        return $cadena;
+    }
 }
